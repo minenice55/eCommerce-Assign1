ef195997af9b00791adacf89c6b3147bd755a3e0
minenice55 <star.elementa@gmail.com>
2022-02-09 20:03:54 -0500

    template files

---
+++ app/views/Home/home.php
@@ -0,0 +1,5 @@
+<?php require APPROOT . '/views/includes/header.php'; 
+?>
+    <h1>Home View</h1>
+    <p>This view is invoked by HomeController</p>
+<?php require APPROOT . '/views/includes/footer.php'; ?>
